6de01941d6e358db5822b4023ac54c71f8817842
fadlyas07 <mhmmdfdlyas@proton.me>
2025-11-15 18:53:13 +0700

    CI: Import clang build scripts
    Change-Id: I0f6e33281fabc1ffbb21d38c2e055c034d255ff9

---
+++ tc_scripts/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# ---- Clang Build Scripts ----
+# Copyright (C) 2023-2025 fadlyas07 <mhmmdfdlyas@proton.me>
+
+# Build LLVM
+export llvm_log="${DIR}/build-llvm-${release_tag}.log"
+./build-llvm.py ${build_flags} \
+    --defines LLVM_PARALLEL_COMPILE_JOBS="${cpu_core}" LLVM_PARALLEL_LINK_JOBS="${cpu_core}" \
+    --build-stage1-only \
+    --build-target distribution \
+    --install-folder "${install_path}" \
+    --install-target distribution \
+    --projects clang lld polly \
+    --llvm-folder "${DIR}/src/llvm-project" \
+    --lto thin \
+    --pgo llvm \
+    --quiet-cmake \
+    --targets ARM AArch64 X86 \
+    --vendor-string "Gf-r${release_tag}" 2>&1 | tee "${llvm_log}"
+
+for clang in "${install_path}"/bin/clang; do
+    if ! [[ -f "${clang}" || -f "${DIR}/build/llvm/instrumented/profdata.prof" ]]; then
+        echo "Building Clang LLVM failed kindly check errors!"
+        exit 1
+    fi
+done
+
+# Execute the push scripts if on the `final` step
+if [[ "${1}" == final ]]; then
+    bash "${DIR}/tc_scripts/push.sh"
+fi
---
+++ tc_scripts/info.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# ---- Clang Build Scripts ----
+# Copyright (C) 2023-2025 fadlyas07 <mhmmdfdlyas@proton.me>
+
+echo -e "[tag]\n${release_tag}" >latest.txt
+release_url="https://github.com/greenforce-project/greenforce_clang/releases/download/${release_tag}/${release_file}"
+touch "clang_notes.txt"
+{
+    echo -e "[date]\n${release_date}\n"
+    echo -e "[clang-ver]\n${clang_version}\n"
+    echo -e "[lld-ver]\n${lld_version}\n"
+    echo -e "[llvm-commit]\n${llvm_url}\n"
+    echo -e "[llvm-commit-msg]\n${lcommit_message}\n"
+    echo -e "[host-glibc]\n${glibc_version}\n"
+    echo -e "[size]\n${release_size}\n"
+    echo -e "[link-rel]\n${release_url}\n"
+    echo -e "[shasum]\n${release_shasum}"
+} >"clang_notes.txt"
+
+touch /tmp/commit_msg
+{
+    echo -e "CI: Bump to ${release_tag} build\n"
+    echo "Clang version: ${clang_version}"
+    echo "LLVM repo commit: ${lcommit_message}"
+    echo "Link: ${llvm_url}"
+} >/tmp/commit_msg
+
+touch get_latest_url.sh
+{
+    echo -e "#!/usr/bin/env bash\n"
+    echo "LATEST_URL=${release_url}"
+} >get_latest_url.sh
